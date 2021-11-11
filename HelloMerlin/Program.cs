b88273bf2d742d161eefb0a578413868931a6f40
domokosgab <domokosgab@gmail.com>
2021-11-11 20:50:38 +0100

    variable declaration and addition

--- HelloMerlin/Program.cs
+++ HelloMerlin/Program.cs
@@ -8,7 +8,14 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); 
+
+            int a = 10;
+            int b = 7;
+
+            int sum = a + b;
+
+            Console.WriteLine(sum);
         }
     }
 }
