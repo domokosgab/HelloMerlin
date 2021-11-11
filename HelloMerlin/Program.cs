2e4ed0bb7c81da5cbbca787b8edd7229fa25aa8c
domokosgab <domokosgab@gmail.com>
2021-11-11 20:55:21 +0100

    comments

--- HelloMerlin/Program.cs
+++ HelloMerlin/Program.cs
@@ -8,13 +8,19 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); 
+            Console.WriteLine("Hello Merlin!"); 
 
-            int a = 10;
+            //egész szám tÍpusú változó deklarációja (a)
+            int a;
+            a = 10;
+
+            //egy második szám deklarációja direkt értékadással (b)
             int b = 7;
 
+            //a két szám összegének tárolása egy változóban (sum)
             int sum = a + b;
 
+            //az összeg kiíratása a konzolra
             Console.WriteLine(sum);
         }
     }
